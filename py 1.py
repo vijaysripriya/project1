4939c52d3585d6952e99aa0dd97b5772d9ea1ecf
vijaysripriya <vijayvijayashwin@gmail.com>
2022-03-28 18:04:28 +0530

    first commit

---
+++ py 1.py
@@ -0,0 +1,23 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("mark list")
+mark list
+name=input("enter first name")
+enter first name vijay
+name1=input("enter last name")
+enter last name vjsp
+n1=int(input("enter tamil marks"))
+enter tamil marks 55
+n2=int(input(" enter computer science"))
+ enter computer science 89
+n2=int(input("enter c++ marks"))
+enter c++ marks 78
+n3=int(input("enter multimedia"))
+enter multimedia 79
+n4=int(input("enter graphics mark"))
+enter graphics mark 89
+n5=int(input("enter web tec mark"))
+enter web tec mark 56
+
+print(n1+n2+n3+n4+n5)
+357
